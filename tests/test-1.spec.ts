cb9c43d7176b69a677cafe01e35c560510ea1fa4
sgapnayr <100978214+sgapnayr@users.noreply.github.com>
2024-07-30 13:45:10 -0500

    test

--- tests/test-1.spec.ts
+++
@@ -1,5 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('https://admindoc.walacor.com/admin-documentation/latest/the-platform-application-an-introduction');
-});
