8abbc369ef7fd456955569190231ca9d9f3bbe11
sgapnayr <100978214+sgapnayr@users.noreply.github.com>
2024-07-30 13:57:08 -0500

    test

--- ciaras-projecta/app/page.tsx
+++
@@ -1,5 +0,0 @@
-import DefaultLayout from '@/layout/default.layout';
-
-export default function Home() {
-  return <DefaultLayout>Walacor App</DefaultLayout>;
-}
--- ciaras-projecta/layout/default.layout.tsx
+++
@@ -1,9 +0,0 @@
-import React, { ReactNode } from "react";
-
-export default function DefaultLayout({ children }: { children?: ReactNode }) {
-  return (
-    <div className="flex min-h-screen w-full flex-col justify-start items-center p-8">
-      {children}
-    </div>
-  );
-}
