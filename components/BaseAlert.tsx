0882fbffb2a0b2831d8d81f34b677952f2afca7d
sgapnayr <100978214+sgapnayr@users.noreply.github.com>
2024-07-31 00:57:37 -0500

    init

--- components/BaseAlert.tsx
+++ components/BaseAlert.tsx
@@ -3,8 +3,8 @@ import React from "react";
 export default function BaseAlert() {
   return (
     <div className="bg-primary text-primary-foreground p-4 text-center text-sm">
-      Welcome to Walacor! Here you can interface with our data platform via your
-      blog posts, profile, and settings.
+      Welcome to Walacor! 🌐 Here you can interface with our data platform via
+      your blog posts, profile, and settings.
     </div>
   );
 }
