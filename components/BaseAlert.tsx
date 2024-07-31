283bf0858175e457e428f669a127e4fa68f9b3d5
sgapnayr <100978214+sgapnayr@users.noreply.github.com>
2024-07-31 00:49:41 -0500

    init

---
+++ components/BaseAlert.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function BaseAlert() {
+  return (
+    <div className="bg-primary text-primary-foreground p-4 text-center text-sm">
+      Welcome to Walacor! Here you can interface with our data platform via your
+      blog posts, profile, and settings.
+    </div>
+  );
+}
--- layout/default.layout.tsx
+++ layout/default.layout.tsx
@@ -1,3 +1,4 @@
+import BaseAlert from "@/components/BaseAlert";
 import BaseFooter from "@/components/BaseFooter";
 import BaseHeader from "@/components/BaseHeader";
 import React, { ReactNode } from "react";
@@ -5,10 +6,7 @@ import React, { ReactNode } from "react";
 export default function DefaultLayout({ children }: { children?: ReactNode }) {
   return (
     <div className="flex flex-col min-h-dvh">
-      <div className="bg-primary text-primary-foreground p-4 text-center text-sm">
-        Welcome to Walacor! Here you can interface with our data platform via
-        your blog posts, profile, and settings.
-      </div>
+      <BaseAlert />
       <BaseHeader />
       <main className="flex-1">{children}</main>
       <BaseFooter />
