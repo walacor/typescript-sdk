1b3769fdc0289c5abecb2f3d605eb9b50ebc3fb0
sgapnayr <100978214+sgapnayr@users.noreply.github.com>
2024-08-29 15:03:12 -0500

    Roles

--- layout/dashboard.layout.tsx
+++ layout/dashboard.layout.tsx
@@ -121,10 +121,10 @@ export default function DashboardLayout({
           <AnimatePresence>
             <motion.div
               key={String(params?.path)}
-              initial={{ opacity: 0, x: 25 }}
-              animate={{ opacity: 1, x: 0 }}
-              exit={{ opacity: 0, x: -25 }}
-              transition={{ duration: 0.25 }}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 1 }}
             >
               {children}
             </motion.div>
