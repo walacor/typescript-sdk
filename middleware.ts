60240bb0c57075f62cdf4326c0e9e24152fa6992
sgapnayr <100978214+sgapnayr@users.noreply.github.com>
2024-11-02 12:16:41 -0500

    Polish

--- middleware.ts
+++ middleware.ts
@@ -9,8 +9,5 @@ export default clerkMiddleware((auth, request) => {
 });
 
 export const config = {
-  matcher: [
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    "/(api|trpc)(.*)",
-  ],
+  matcher: ["/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)", "/(api|trpc)(.*)"],
 };
